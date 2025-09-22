61911fa20cd31b260f33ccf891f356c4fd6721df
lucas2906 <lucas.ringer2006@gmail.com>
2025-09-22 17:00:20 +0200

    Datum absteigend sortieren implementiert

--- JS/EventSortierung.js
+++ JS/EventSortierung.js
@@ -1,6 +1,12 @@
 function sortDateascending() {
     const list = document.getElementById("events");
-    
+    const items = list.getElementsByTagName("li");
+    const itemsArr = Array.from(items);
+    itemsArr.sort((a, b) => {
+        const dateA = new Date(a.getAttribute("data-date"));
+        const dateB = new Date(b.getAttribute("data-date"));
+        return dateA - dateB;
+    }
 }
 function sortDatedescending() {}
 function sortTitleascending() {}
