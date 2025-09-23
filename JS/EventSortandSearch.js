f5b9532b104cb38c2554990e32fb29a7add9907b
lucas2906 <lucas.ringer2006@gmail.com>
2025-09-23 14:37:35 +0200

    Sortierung nach Datum benötigt eine andere Logik

--- JS/EventSortandSearch.js
+++ JS/EventSortandSearch.js
@@ -3,11 +3,24 @@ function sortDateascending() {
     const items = list.getElementsByTagName("li");
     const itemsArr = Array.from(items);
     itemsArr.sort((a, b) => {
-        const dateA = new Date(a.getAttribute("data-date"));
-        const dateB = new Date(b.getAttribute("data-date"));
-        return dateA - dateB;
+        
+        
     });}
+
+function sortDatedescending() {
+    const list = document.getElementById("events");
+    const items = list.getElementsByTagName("li");
+    const itemsArr = Array.from(items);
+    itemsArr.sort((a, b) => {
+        
     
-function sortDatedescending() {}
+});}
+
 function sortTitleascending() {}
 function sortTitledescending() {}
+
+function searchEvent() {
+    const input = document.getElementById("searchEvent").value.toLowerCase();
+    const list = document.getElementById("events");
+    const items = list.getElementsByTagName("li");
+}
